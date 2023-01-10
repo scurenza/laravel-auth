730ec0582f439ac52b07b6498f51ef35604e0c15
scurenza <fedechiar@hotmail.it>
2023-01-10 20:45:49 +0100

    first commit

---
+++ resources/views/admin/projects/index.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="container mt-4">
+        <h3 class="text-center">La lista dei progetti</h3>
+        <div class="row justify-content-center">
+            <div class="col-8">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Titolo</th>
+                            <th scope="col">Data di creazione</th>
+                            <th scope="col">Azioni</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($projects as $project)
+                            <tr>
+                                <th scope="row">{{ $project->title }}</th>
+                                <td>{{ $project->created_at }}</td>
+                                <td>
+                                    <a class="btn btn-success" href="{{ route('admin.projects.show', $project->slug) }}">
+                                        <i class="fa-solid fa-eye"></i>
+                                    </a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+@endsection
